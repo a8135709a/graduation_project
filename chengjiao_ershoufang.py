9d423bbfb294f19d121bbb8d8cab39ddb49552f9
gost <a8135709a@hotmail.com>
2017-04-26 13:23:06 +0800

    0426,final version

--- chengjiao_ershoufang.py
+++ chengjiao_ershoufang.py
@@ -1,9 +1,10 @@
-from bs4 import BeautifulSoup
-import requests
-import pymongo
 import multiprocessing
 import time
 import re
+from bs4 import BeautifulSoup
+import requests
+import pymongo
+
 
 headers = {
     'Connection': 'keep-alive',
@@ -28,12 +29,12 @@ def get_url():
     url = 'https://dl.lianjia.com/chengjiao/'
     wb_data = requests.get(url, headers=headers)
     wb_data.encoding = 'utf-8'
-    soup = BeautifulSoup(wb_data.text,'lxml')
+    soup = BeautifulSoup(wb_data.text, 'lxml')
     area_link = soup.select('.position > dl > dd > div > div > a')
     for i in area_link:
         area_links.append('https://dl.lianjia.com{}'.format(i.get('href')))
-    # print(area_links)
-# get_url()
+    print(area_links)
+    print('\n完成，请执行第二步\n')
 
 all_area_links = []
 
@@ -66,8 +67,8 @@ def get_all_url():
                 'where': a
             }
             area_list.insert_one(data)
-            # print(data)
-# get_all_url()
+            print(data)
+    print('\n完成，请执行第三步\n')
 
 
 def get_all_page_url():
@@ -86,7 +87,7 @@ def get_all_page_url():
                 }
                 all_page_url.insert_one(data)
                 print(data)
-# get_all_page_url()
+    print('\n完成，请执行第四步\n')
 
 page_db_url = [url['url'] for url in all_page_url.find()]
 page_index_url = [url['father_url'] for url in all_house_url.find()]
@@ -102,16 +103,23 @@ def get_all_house_url():
         date = soup.select('.dealDate')
         link = soup.select('.info > .title > a')
         title = soup.select('.info > .title > a')
-        for d, l, t in zip(date, link, title):
-            data = {
-                'url': l.get('href'),
-                'father_url': i,
-                'area': [area['area'] for area in all_page_url.find({'url': i})][0],
-                'dealDate': d.get_text(),   # 成交日期
-                'title': t.get_text()
-            }
-            print(data)
-            all_house_url.insert_one(data)
+        if soup.select('title')[0].text == '414 Request-URI Too Large':
+            print('请打开网页输入验证码，程序将在15秒后继续执行', i)
+            time.sleep(15)
+        elif soup.find('div', attrs={'class': 'icon-404 icon fl'}):
+            print(i, '页面不存在')
+        else:
+            for d, l, t in zip(date, link, title):
+                data = {
+                    'url': l.get('href'),
+                    'father_url': i,
+                    'area': [area['area'] for area in all_page_url.find({'url': i})][0],
+                    'dealDate': d.get_text(),   # 成交日期
+                    'title': t.get_text()
+                }
+                print(data)
+                all_house_url.insert_one(data)
+    print('\n完成，请执行第五步\n')
 
 house_db_url = [url['url'] for url in all_house_url.find()]
 house_index_url = [url['url'] for url in all_house_info.find()]
@@ -125,26 +133,58 @@ def get_house_info():
         wb_data = requests.get(i, headers=headers)
         soup = BeautifulSoup(wb_data.text, 'lxml')
         # time.sleep(2)
-        data = {
-            'url': i,  # 链接
-            'title': [title['title'] for title in all_house_url.find({'url': i})][0],  # 标题
-            'area': [area['area'] for area in all_house_url.find({'url': i})][0],  # 区域
-            'price': soup.select('.dealTotalPrice > i')[0].text,  # 价格
-            'unitPrice': soup.select('.info.fr > .price > b')[0].text,  # 平米价
-            'dealCycle': soup.select('.msg > span > label')[1].text,  # 成交周期
-            'see': soup.select('.msg > span > label')[3].text,  # 带看（次）
-            'follow': soup.select('.msg > span > label')[4].text,  # 关注（人）
-            'browse': soup.select('.msg > span > label')[5].text,  # 浏览（次）
-            'saleDate': re.sub(' ', '', re.sub('挂牌时间', '', soup.select('.transaction > div > ul > li')[2].text)),  # 挂牌时间
-            'pattern': re.sub(' ', '', re.sub('房屋户型', '', soup.select('.base > .content > ul > li')[0].text)),  # 房屋户型
-            'spaceSize': re.sub(' ', '', re.sub('建筑面积', '', soup.select('.base > .content > ul > li')[2].text))   # 建筑面积
-        }
-        print(data)
-        all_house_info.insert_one(data)
-# get_house_info()
+        if soup.select('title')[0].text == '414 Request-URI Too Large':
+            print('请打开网页输入验证码，程序将在15秒后继续执行', i)
+            time.sleep(15)
+        elif soup.find('div', attrs={'class': 'icon-404 icon fl'}):
+            print(i, '页面不存在')
+        else:
+            data = {
+                'url': i,  # 链接
+                'title': [title['title'] for title in all_house_url.find({'url': i})][0],  # 标题
+                'area': [area['area'] for area in all_house_url.find({'url': i})][0],  # 区域
+                'price': soup.select('.dealTotalPrice > i')[0].text,  # 价格
+                'unitPrice': soup.select('.info.fr > .price > b')[0].text,  # 平米价
+                'dealCycle': soup.select('.msg > span > label')[1].text,  # 成交周期
+                'see': soup.select('.msg > span > label')[3].text,  # 带看（次）
+                'follow': soup.select('.msg > span > label')[4].text,  # 关注（人）
+                'browse': soup.select('.msg > span > label')[5].text,  # 浏览（次）
+                'saleDate': re.sub(' ', '', re.sub('挂牌时间', '', soup.select('.transaction > div > ul > li')[2].text)),  # 挂牌时间
+                'pattern': re.sub(' ', '', re.sub('房屋户型', '', soup.select('.base > .content > ul > li')[0].text)),  # 房屋户型
+                'spaceSize': re.sub(' ', '', re.sub('建筑面积', '', soup.select('.base > .content > ul > li')[2].text))   # 建筑面积
+            }
+            print(data)
+            all_house_info.insert_one(data)
+    print('\n所有数据爬取成功！\n')
 
-if __name__ == "__main__":
+
+def main():
+    print('请按步骤执行并注意屏幕输出的提示：\
+    \n1.first step\n2.second step\n3.third step\n4.fourth step\n5.fifth step\n0.exit')
+    a = input('输入你的选择(1、2、3、4、5、0)：')
+    if a == '1':
+        get_url()
+        main()
+    elif a == '2':
+        get_all_url()
+        main()
+    elif a == '3':
+        get_all_page_url()
+        main()
+    elif a == '4':
+        get_all_house_url()
+        main()
+    elif a == '5':
+        get_house_info()
+    elif a == '0':
+        exit()
+    else:
+        print('error')
+        exit()
+
+
+if __name__ == '__main__':
     pool = multiprocessing.Pool()
-    pool.apply_async(get_house_info)
+    pool.apply_async(main())
     pool.close()
     pool.join()
